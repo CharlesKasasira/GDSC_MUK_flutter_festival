3929ebd212e377b294ee8a5450d0b2c2fb5940f7
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-12 10:21:44 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(Festival());
+}
+
+class Festival extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "This is our festival",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Festival 2022"),
+          backgroundColor: Colors.green,
+        ),
+        body: Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, rowNumber) {
+          return Column(
+            children: [
+              Image.asset("assets/pic.png"),
+              Text("This is Row number $rowNumber")
+            ],
+          );
+        });
+  }
+}
+
+
+
+// Me tryingout Flutter.
+
+// Flutter
+/* import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    Center(
+      child: Text(
+        'Kasasira',
+        textDirection: TextDirection.rtl,
+      ),
+    ),
+  );
+} */
+
+// arrow like functions in dart
+
+
